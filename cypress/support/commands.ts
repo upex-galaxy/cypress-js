f4ebaa0dadc3806de7c828f3ca39f81fa5b81546
Lutome <lucas.t.medina1994@gmail.com>
2024-09-22 22:09:40 -0700

    cached issues resolved, commands.ts was ran instead gx3-5140

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef*/
 // ***********************************************
 // https://on.cypress.io/custom-commands
 // ***********************************************
@@ -6,12 +6,13 @@ import { SpaceCheckoutPage } from '@pages/SpaceCheckout.Page';
 import { SpaceLoginPage } from '@pages/SpaceLogin.Page';
 import { SpaceDestinationPage } from '@pages/SpaceProduct.Page';
 import 'cypress-file-upload';
-
+/*
 beforeEach(() => {
 	Cypress.on('uncaught:exception', () => false); // returning false here prevents Cypress from failing the test
 	cy.intercept({ resourceType: /^(xhr|fetch)$/ }, { statusCode: 200, body: { data: 'fake data' } });
 	cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
 });
+*/
 
 //* Este es el commando principal para usar los Page Object Models sin necesidad de importarlos en cada archivo de test
 Cypress.Commands.add('page', () => {
